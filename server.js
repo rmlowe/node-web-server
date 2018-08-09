a4abe8017679f450f5b3ad7dd1b0e4771f754ca1
Robert Lowe <robertmlowe@rmlowe.com>
2018-08-09 15:23:19 +0800

    Rendering Templates with Data

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Andrew',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 // /bad - send back json with errorMessage
